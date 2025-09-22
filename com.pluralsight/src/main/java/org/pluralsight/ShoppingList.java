a67dc3822e253c63e177a0591f92eedda7276d60
Student <nigelmraydavey@gmail.com>
2025-09-22 14:46:46 -0700

    Working with Intellij. Shopping List

---
+++ com.pluralsight/src/main/java/org/pluralsight/ShoppingList.java
@@ -0,0 +1,24 @@
+package org.pluralsight;
+
+public class ShoppingList {
+    static void main() {
+        System.out.println("rice");
+        System.out.println("Soysauce");
+        System.out.println("Cheese");
+        System.out.println("Tea");
+        System.out.println("Bread");
+        System.out.println("Ham");
+        System.out.println("Milk");
+        System.out.println("Salmon");
+        System.out.println("Seaweed");
+        System.out.println("Crackers");
+
+
+
+
+
+
+
+
+    }
+}
