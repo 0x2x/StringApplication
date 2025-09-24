4dd66ecb65682136bd1c7fbf02e3f41db13333c4
Student <nigelmraydavey@gmail.com>
2025-09-24 13:58:19 -0700

    First Commit

---
+++ StringApplication/src/main/java/org/pluralsight/Reciept.java
@@ -0,0 +1,14 @@
+package org.pluralsight;
+
+public class Reciept {
+    static void main() {
+        System.out.println();
+
+        String item = "Choclate";
+        double item_price = 2.99;
+        int quanity = 15;
+        System.out.println("I bought " + quanity + " " + item + " " + "for " + "$" + item_price);
+
+
+    }
+}
---
+++ StringApplication/src/main/java/org/pluralsight/VariableApp.java
@@ -0,0 +1,28 @@
+package org.pluralsight;
+
+public class VariableApp {
+
+    static void main() {
+        String FavoriteColor = "Blue";
+        int StartedClass = 2025;
+        char MiddleInital = 'M';
+        boolean Pets = true;
+        String message = "You look amazing today!";
+
+        System.out.println("My Favorite color is " + FavoriteColor);
+        System.out.println("Started yearup in year " + StartedClass);
+        System.out.println("My middle initial is " + MiddleInital);
+        System.out.println("Do I have pets " + Pets);
+        System.out.println("My Compliment for you today is " + message);
+
+        int daysInOneWeek = 5;
+        double PriceOfCoffee = 4.99;
+        char MyFavoriteLetter = 'M';
+        boolean IsItRaining = true;
+        System.out.println("There is " + daysInOneWeek + " amount of days in one week.");
+        System.out.println("I bought this coffee for " + PriceOfCoffee + " can you believe that!");
+        System.out.println("My favorite letter as of right now has to be " + MyFavoriteLetter + " as my best friends start with that letter");
+        System.out.println("Is it raining outside " + IsItRaining);
+
+    }
+}
