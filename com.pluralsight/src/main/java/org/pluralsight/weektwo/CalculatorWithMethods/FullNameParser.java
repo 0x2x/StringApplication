e60c2431b5cc1065f50c035700f9929150d3d4c7
Student <nigelmraydavey@gmail.com>
2025-09-30 11:59:16 -0700

    Working with Intellij. Some Challenges. - week 2

--- com.pluralsight/src/main/java/org/pluralsight/weektwo/CalculatorWithMethods/FullNameParser.java
+++ com.pluralsight/src/main/java/org/pluralsight/weektwo/CalculatorWithMethods/FullNameParser.java
@@ -1,5 +1,8 @@
 package org.pluralsight.weektwo.CalculatorWithMethods;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class FullNameParser {
@@ -11,9 +14,11 @@ public class FullNameParser {
         System.out.print("Full name: ");
 
         String FullName = scan.nextLine();
+        String FullNameTrim = FullName.trim();
 
+        String[] FullNameSplit = FullNameTrim.split(" ");
 
-        String[] FullNameSplit = FullName.split(" ");
+        // method one
         if (FullNameSplit.length == 3) {
             // contains first, middle, last
             System.out.println("First name: " + FullNameSplit[0]);
@@ -24,6 +29,34 @@ public class FullNameParser {
             System.out.println("First name: " + FullNameSplit[0]);
             System.out.println("Last name: " + FullNameSplit[1]);
         }
+        // method two
+
+        // getting first word and last word
+        String FirstName = FullNameSplit[0];
+        int LastNameIndex = FullNameSplit.length - 1;
+        String LastName = FullNameSplit[LastNameIndex];
+        String MiddleName = "(NONE)";
+        if(FullNameSplit.length > 2) {
+            List<String> list = new ArrayList<String>();
+            for (String name : FullNameSplit) {
+                list.add(name);
+            }
+
+            list.removeFirst();
+            list.remove(LastNameIndex - 1);
+
+            System.out.println("First name: " + FirstName);
+            System.out.println("Middle names: " + String.join(" ", list));
+            System.out.println("Last name: " + LastName);
+
+        }else {
+            System.out.println("First name: " + FirstName);
+            System.out.println("Middle name: " + MiddleName);
+            System.out.println("Last name: " + LastName);
+
+        }
+
+
 
     }
 }
